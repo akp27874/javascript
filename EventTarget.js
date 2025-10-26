0fe1d2088bf9129beb80f93f0aac025dc8f5172e
Ashish <akp27874@gmail.com>
2025-10-26 16:58:18 +0530

    Added EventTargetVsEventCurrentTarget.html in topics

--- EventTarget.js
+++
@@ -1,4 +0,0 @@
-document.getElementById('menu').addEventListener('click', (event)=>{
-    console.log('Clicked Item : ', event.target.textContent);
-    console.log('Current Item : ', event.currentTarget);
-})
