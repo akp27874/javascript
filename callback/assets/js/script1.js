d515cc94651afa13eca312b60a3c14751fedcf7e
Ashish <akp27874@gmail.com>
2025-10-26 19:43:20 +0530

    Updated index.html

--- callback/assets/js/script1.js
+++ callback/assets/js/script1.js
@@ -3,18 +3,3 @@
 * Call back is higher order functions in javascript
 */
 
-
-var sachin = function(){
-    console.log("My name is Sachin and I called inside a function");
-}
-
-var callHero = function(sachin){
-    console.log("+++Before Adding Heros+++");
-    sachin();
-}
-
-/* Case 1 */
-//callHero(); // error It is expecting an argument "Uncaught TypeError: sachin is not a function"
-
-/* Case 2 */
-callHero(sachin);
